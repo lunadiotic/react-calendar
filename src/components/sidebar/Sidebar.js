7ec12112a70f6d3319b5224c769d20e9f27005dc
aimeliala <ahmadhakim404@gmail.com>
2022-01-22 16:26:04 +0700

    make sidebar component

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,7 +3,23 @@ import React from 'react';
 const Sidebar = (props) => {
     return (
         <div className='col-3'>
-            Create new event
+            <div class="d-grid gap-2">
+                <button class="btn btn-primary" type="button">
+                    Create New Event
+                </button>
+            </div>
+            <div className="m-t-20">
+                <br />
+                <div className="my-1 p-2 bg-primary">
+                    Watch Movies
+                </div>
+                <div className="my-1 p-2 bg-success">
+                    Learn
+                </div>
+                <div className="my-1 p-2 bg-danger">
+                    Hangout
+                </div>
+            </div>
         </div>
     );
 };
