1111b7aa08f028931a7218cc777644b6443ac3ad
aim <ahmadhakim404@gmail.com>
2022-01-30 03:23:27 +0700

    design form event modal

--- src/components/modal/EventForm.js
+++ src/components/modal/EventForm.js
@@ -1,18 +1,58 @@
 const EventForm = () => {
     return (
-        <div class="modal" tabindex="-1" id="add-event">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div className="modal" tabindex="-1" id="add-event">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title">Event Form</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
+                <div className="modal-body p-3">
+                    <form action="">
+                        <div className="mb-3">
+                            <label htmlFor="" className="form-label">Event Title</label>
+                            <input 
+                                type="text"
+                                name="event-title"
+                                placeholder="Enter Title" 
+                                className="form-control" />
+                        </div>
+                        <div className="mb-3 form-check">
+                            <input 
+                                type="checkbox"
+                                name="checkbox"
+                                placeholder="Enter Title" 
+                                className="form-check-input" />
+                            <label htmlFor="" className="form-label">All-day event? (Optional)</label>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="" className="form-label">Start</label>
+                            <input 
+                                type="date"
+                                name="date-start"
+                                className="form-control" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="" className="form-label">End</label>
+                            <input 
+                                type="date"
+                                name="date-end"
+                                className="form-control" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="" className="form-label">Choose Event Color</label>
+                            <select name="" id="" className="form-control">
+                                <option value="">Select Color</option>
+                                <option value="">Primary</option>
+                                <option value="">Info</option>
+                                <option value="">Danger</option>
+                            </select>
+                        </div>
+                    </form>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary">Save changes</button>
                 </div>
                 </div>
             </div>
